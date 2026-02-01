8533c6146d6ad261e74b58e08c3323e2ba042401
Pallav Bhattarai <pallavbhattarai1995@gmail.com>
2026-02-01 20:05:39 +0545

    Your descriptive commit message

--- charwithname.c
+++ charwithname.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main(){
-    
+
     char name[10];
     printf("enter your name : ");
     scanf( "%[^/n]", name);
     printf("your entered name is :- %s", name);
+    
     return 0;
 }
